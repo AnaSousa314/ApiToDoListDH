45d3c0d5f62e96a66863c6c42ee086c1ea3f8120
Ana.Sousa <anasousa314@outlook.com>
2021-04-10 13:19:37 -0300

    adiciona routes.js para as rotas

---
+++ src/routes/routes.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+
+// outra forma
+
+/* 
+const {Router} = require('express');
+const router = Router();
+
+*/
+
+
+
+module.exports = router
