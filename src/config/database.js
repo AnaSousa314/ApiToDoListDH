3ea331ec82727fed5ff6d3b67ea77374840c2bb5
Ana.Sousa <anasousa314@outlook.com>
2021-04-12 09:55:00 -0300

    Mostra como tirar o timestamp de todo o projeto

---
+++ src/config/database.js
@@ -0,0 +1,14 @@
+const config = {
+  username: 'root',
+  password: '',
+  database:'api_to_do_list',
+  host: 'localhost',
+  dialect: 'mysql',
+ /*  define:{
+    timestamp: false
+  } essa definição é aplicada a todos os models */
+}
+
+// console.log(config);
+
+module.exports = config;
